754691e00692b7e2da39d9f75a89c6d3c0a67a3b
Blockchain8318 <suraj.prajapati@mail.vinove.com>
2023-09-11 18:21:42 +0530

    portfolio created & basic setup done

---
+++ src/Components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
+      <div className="container px-5">
+        <Link className="navbar-brand" to="/">
+          <span className="fw-bolder text-primary">Suraj Prajapati</span>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/resume">
+                Resume
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/projects">
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar
